f9390f5fd543f1b579b5dc5164a833ae7483b7e1
Sshovon <frcshovon@gmail.com>
2022-08-13 08:12:16 +0600

    patient check

--- api/src/routes/patientRoutes.js
+++ api/src/routes/patientRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 
 const Patient = require('../models/patientModel')
-
+const Appointment = require('../models/appointmentModel')
 
 router.post('/register',async(req,res)=>{
     try{
@@ -17,4 +17,22 @@ router.post('/register',async(req,res)=>{
     }
 })
 
+router.post('/check',async(req,res)=>{
+    try{
+        const response={}
+        const {nid,ID} = req.body;
+        const patient = await Patient.find({nid});
+        const appointment = await Appointment.find({ID});
+        if(!patient.length)
+            response.error="patient is not registered";
+        else if(!appointment.length){    
+            response.error="appointment not found";
+        }else{
+            response.success=true
+        }
+        res.send(response)
+    }catch(e){
+        res.send({error:e.message})
+    }
+})
 module.exports = router
